6892274d31159d12ce7474797073e97bfa7d0ae4
pawaryas <yash.cs1993@mitindore.co.in>
2024-04-19 11:43:39 +0530

    backend code

--- backend/app.js
+++ backend/app.js
@@ -13,11 +13,11 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUploader());
-app.use(express.static(path.join(__dirname, "../frontend/build")))
+// app.use(express.static(path.join(__dirname, "../frontend/build")))
 
-app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"))
-})
+// app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"))
+// })
 
 const corsOptions = {
     origin: 'http://localhost:3000',
