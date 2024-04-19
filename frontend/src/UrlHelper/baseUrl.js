3b2a2482dedac6ee92104c30a581ea459a994d16
pawaryas <yash.cs1993@mitindore.co.in>
2024-04-19 12:10:21 +0530

    code...

--- frontend/src/UrlHelper/baseUrl.js
+++ frontend/src/UrlHelper/baseUrl.js
@@ -1,2 +1,4 @@
-export const baseUrl = "http://localhost:4000/api/v1";
+// export const baseUrl = "http://localhost:4000/api/v1";
 // export const baseUrl = "/api/v1";
+
+export const baseUrl = "https://my-ecommerce-backend-c7xg.onrender.com/api/v1";
